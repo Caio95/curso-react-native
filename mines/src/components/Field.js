fa44057522c5552c1cad1f8fde93db58a7973e24
Caio95 <caiohenrique1701@gmail.com>
2020-04-01 16:14:23 -0300

    jogo de minas

---
+++ mines/src/components/Field.js
@@ -0,0 +1,8 @@
+import React from 'react' //sempre que tiver JSX
+import { View, StyleSheet } from 'react-native'
+import params from '../params'
+
+export default props => {
+    const styleField = [styles.field]
+    if(styleField.length === 1) styleField.push(styles.regular)
+}
